8144f370b03edfe9843f2c13c04d7f82f70fac3e
Elchanan Solomon <isaacsolomonla@gmail.com>
2018-08-03 12:42:26 -0400

    Add files via upload

--- analyze_devan.py
+++ analyze_devan.py
@@ -1,21 +1,21 @@
-# -*- coding: utf-8 -*-
+
 """
 Analyzing and Classifying Devanagari Characters Dataset using euler characteristic curves
 
 @author: Elchanan
 """
 
+  
 import ECC
 import numpy as np
 import matplotlib.pyplot as plt
 #Sample 8 directions on the unit circle
 directions = [(1,0),(0,1),(1/1.414,1/1.414)]
 #directions = [(np.cos(x),np.sin(x)) for x in np.linspace(0,np.pi/2,16)]
-#some sample directions
 #directions = [(1,0),(0,1),(-1,0),(0,-1),(1,1),(1,-1),(-1,1),(-1,-1),(2,1),(2,-1),(-2,1),(-2,-1)]
 #directions = [(np.cos(x),np.sin(x)) for x in np.linspace(0,np.pi,4)]
 
 #Predict labels for 100 test images
-labels = ECC.predict_labels(devan_train_figures,devan_train_labels,devan_test_figures[:180],directions,1)
+labels = ECC.predict_labels(devan_train_figures,devan_train_labels,devan_test_figures,directions,1)
 #Display the score
-print('Accuracy on the test set: ' + str(sum(np.array(labels) == np.array(devan_test_labels[:180]))/180.0) +'%' )
+print('Accuracy on the test set: ' + str(sum(np.array(labels) == np.array(devan_test_labels))/(len(devan_test_figures) + 0.0)) +'%' )
